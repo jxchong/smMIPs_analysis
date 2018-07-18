b515d0b86f14cfffc90d595a73ea44e2351571ee
Jessica Chong <jxchong@uw.edu>
2018-07-18 11:45:48 -0700

    fix another typo

--- create_MIPs_analysis_files.pl
+++ create_MIPs_analysis_files.pl
@@ -236,7 +236,7 @@ sub makeSampleKey {
 			} else {
 				die "The samplesheet $samplesheet has a format I cannot handle (possibly: not csv, not tab-delimited, doesn't have 4 columns (Sample_ID,Sample_Name,GenomeFolder,index) in the sample section)\n";
 			}
-			$sampleentry[1] =~ s/_/-/g);
+			$sampleentry[1] =~ s/_/-/g;
 			if ($1 ne '') {
 				print "IMPORTANT!! The MiSeq software automatically replaces all underscore characters with hyphens but does not warn you about this. This script automatically generates a sample key with the underscores replaced as well to match with the MiSeq fastq outputs.\n";
 			}
